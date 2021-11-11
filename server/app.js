86c4beb433dac0005b973a97ab8dc89b2d552c53
Aure-M <marcaurelalioza@gmail.com>
2021-11-11 16:15:16 +0100

    Update

--- server/app.js
+++ server/app.js
@@ -1,14 +1,19 @@
-const express=require('express');
+const express = require('express');
 const bodyParser=require('body-parser');
 const path=require("path");
-const clientPath="/home/aurel-m02/Bureau/ProjetWeb/client"
+const clientPath="/home/aurel-m02/Bureau/ProjetWeb/client";
+
 
 const app=express();
 
+app.use(express.static(clientPath))
 
-app.use('/',(req,res,next)=>{
+app.get('/', (req, res) =>{
+    res.sendFile(clientPath+'/index.html')
+})
+
+/* app.use('/',(req,res,next)=>{
     res.status(200).sendFile("index.html",{root: path.join(clientPath)});
-});
-
+}); */
 
 module.exports=app;
