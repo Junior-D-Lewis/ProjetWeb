19327bfb290cecd5ad254089d6de5d422e4f4734
BlackStar01 <76823098+BlackStar01@users.noreply.github.com>
2021-11-13 22:04:28 +0100

    Update index.js

--- client/js/index.js
+++ client/js/index.js
@@ -1,18 +1,9 @@
 
-<<<<<<< Updated upstream
-const Register = window.httpVueLoader('../components/Register.vue')
-const Login = window.httpVueLoader('../components/Login.vue')
-const Modal = window.httpVueLoader('./components/Modal.vue')
-
-const routes = [
-  { path: '/register', component: Modal },
-=======
 const Register = window.httpVueLoader('./components/Register.vue')
 const Modal = window.httpVueLoader('./components/Modal.vue')
 
 const routes = [
   { path: '/register', component: Register },
->>>>>>> Stashed changes
   { path: '/login', component: Modal }
 ]
 
@@ -21,37 +12,10 @@ const router = new VueRouter({
 })
 
 const app = new Vue({
-<<<<<<< Updated upstream
   el: "#app",
   router,
   data: {
     advantages: [
-=======
-    el: "#app",
-    router,
-
-    data: {
-        advantages: [
-            {
-                url: "./img/scissors.png",
-                title: "Utilité et utilité",
-                text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe ipsa eveniet alias!"
-            },
-            {
-                url: "./img/scissors.png",
-                title: "Utilité et utilité",
-                text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe ipsa eveniet alias!"
-            },
-            {
-                url: "./img/scissors.png",
-                title: "Utilité et utilité",
-                text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe ipsa eveniet alias!"
-            }
-        ]
-    }, 
-    methods: {
-      simulateClick()
->>>>>>> Stashed changes
       {
         url: "./img/scissors.png",
         title: "Utilité et utilité",
