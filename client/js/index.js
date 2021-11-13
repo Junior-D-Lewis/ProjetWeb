a4cb1fc740e774c41f3fb4cd1750aab050bb4fa5
lewis3215 <juniordjomo07@gmail.com>
2021-11-13 14:05:43 +0100

    register&login

--- client/js/index.js
+++ client/js/index.js
@@ -1,33 +1,8 @@
-/* const Modal = window.httpVueLoader('../components/modal.vue')
- */
-
-/* Temporaire */
-const Modal = 
-{ 
-    template: `<a href="#id01">Ouvrir la fenêtre modale</a>
-
-    <div id="id01" class="modal">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <header class="container">
-            <a href="#" class="closebtn">×</a>
-            <h2>Entête de modale</h2>
-          </header>
-          <div class="container">
-            <p>Texte dans la fenêtre modale.</p>
-          </div>
-          <footer class="container">
-            <p>Pied de page de modale</p>
-          </footer>
-        </div>
-      </div>
-    </div>
-    ` 
-}
-const Login = { template: '<div>bar</div>' }
+const Register = window.httpVueLoader('../components/Register.vue')
+const Login = window.httpVueLoader('../components/Login.vue')
 
 const routes = [
-    { path: '/register', component: Modal },
+    { path: '/register', component: Register },
     { path: '/login', component: Login }
 ]
 
@@ -38,9 +13,6 @@ const router = new VueRouter({
 const app = new Vue({
     el: "#app",
     router,
-    /* components: { 
-        Modal
-    }, */
     data: {
         advantages: [
             {
